51f19589abf8a9b8123c3f11d4d2047687d31d2e
Josephine Liang <onejosefina@gmail.com>
2018-08-24 16:25:09 -0700

    use the heroku-addon keys

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,5 +1,5 @@
 AlgoliaSearch.configuration = {
-    application_id: Figaro.env.algolia_api_id,
-    api_key: Figaro.env.algolia_api_key,
+    application_id: Figaro.env.algoliasearch_application_id,
+    api_key: Figaro.env.algoliasearch_api_key,
     pagination_backend: :will_paginate
 }
