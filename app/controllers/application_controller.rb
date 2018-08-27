f4ded1cd232b49d11fc219519b4bb7ba72567af5
Josephine Liang <onejosefina@gmail.com>
2018-08-27 10:30:44 -0700

    add reindex call in application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  Rails.application.eager_load! # Ensure all models are loaded(required in development).
+  algolia_models = ActiveRecord::Base.descendants.select{ |model| model.respond_to?(:reindex) }
+  algolia_models.each(&:reindex)
 end
