89b76f0906f4b943542e03a752de1a4099ab6ed9
Josephine Liang <onejosefina@gmail.com>
2018-08-27 10:50:43 -0700

    remove the change_country name migration

--- db/migrate/20180824233237_change_country_name.rb
+++
@@ -1,5 +0,0 @@
-class ChangeCountryName < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :contacts, :country, :county
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_24_205708) do
-
-  create_table "Contacts", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "company"
-    t.string "address"
-    t.string "city"
-    t.string "county"
-    t.string "state"
-    t.string "zip"
-    t.string "phone"
-    t.string "fax"
-    t.string "web"
-    t.integer "followers"
-    t.string "note"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
