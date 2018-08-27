f481f280779f6fdcb1925a646663a7db4301e736
Josephine Liang <onejosefina@gmail.com>
2018-08-27 10:55:57 -0700

    did migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_08_24_203942) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "company"
+    t.string "address"
+    t.string "city"
+    t.string "county"
+    t.string "state"
+    t.string "zip"
+    t.string "phone"
+    t.string "fax"
+    t.string "web"
+    t.integer "followers"
+    t.string "note"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
